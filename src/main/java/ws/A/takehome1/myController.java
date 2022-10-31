a987c3760835a01c5f20693e080510e5bd8b5f5b
akmal <akmal@DESKTOP-QEPVMLM>
2022-10-31 11:26:20 +0700

    Commit

--- src/main/java/ws/A/takehome1/myController.java
+++ src/main/java/ws/A/takehome1/myController.java
@@ -5,19 +5,13 @@
 package ws.A.takehome1;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import org.springframework.core.io.ByteArrayResource;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  *
@@ -27,41 +21,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class myController {
     @RequestMapping("/takehome")
-//    @ResponseBody
-    
     public String Inputan
         (
             @RequestParam(value="Nama") String inputNama,
             @RequestParam(value="Lokasi") String inputLokasi,
-            @RequestParam(value="Gambar") String inputGambar,
+            @RequestParam(value="Gambar") MultipartFile inputGambar,
             Model input
-        )
-    {
-        input.addAttribute("Nama", inputNama);
-        input.addAttribute("Lokasi", inputLokasi);
-        input.addAttribute("Gmabar", inputLokasi);
+        ) throws IOException
         
-        
-        return "viewpage";
-    }
-        
-    @RequestMapping(value = "getimage/{photo}", method = RequestMethod.GET)
-    @ResponseBody
-    public ResponseEntity<ByteArrayResource> getImage(@PathVariable("photo")String photo) throws IOException{
-        if (!photo.equals("")|| photo !=null) {
-            try{
-                Path filename = Paths.get("uploads",photo);
-                byte[] buffer = Files.readAllBytes(filename);
-                ByteArrayResource byteArrayResource = new ByteArrayResource(buffer);
-                return ResponseEntity.ok()
-                        .contentLength(buffer.length)
-                        .contentType(MediaType.parseMediaType("image/png"))
-                        .body(byteArrayResource);
-            }catch (Exception e) {
-            }
+        {
+            byte[] img  = inputGambar.getBytes();
             
+                    
+            return "view";
         }
-        return ResponseEntity.badRequest().build();
-    }
-    
 }
